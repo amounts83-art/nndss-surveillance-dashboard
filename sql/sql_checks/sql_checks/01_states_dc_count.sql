4ec362af2df35dc15ec5f4204491313fdc18ce6d
amounts83-art <amounts83@gmail.com>
2026-03-02 09:45:41 -0500

    Delete sql/sql_checks/sql_checks directory

--- sql/sql_checks/sql_checks/01_states_dc_count.sql
+++
@@ -1,5 +0,0 @@
--- Expect: 51 distinct jurisdictions (50 states + DC)
-SELECT
-  COUNT(DISTINCT state_name) AS n_states_dc
-FROM `public_health_portfolio.cdc_nndss_weekly_curated_v2`
-WHERE geo_level = 'state';
