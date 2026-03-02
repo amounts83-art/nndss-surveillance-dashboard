774e3b5957b2be3473924fb3abb84fa4ed4a3810
amounts83-art <amounts83@gmail.com>
2026-03-02 09:36:28 -0500

    Delete sql/sql_checks/sql_checks/sql_checks directory

--- sql/sql_checks/sql_checks/sql_checks/sql_checks/sql_checks/04_export_rowcount_matches_fact.sql
+++
@@ -1,4 +0,0 @@
--- Expect: export_rows = fact_rows
-SELECT
-  (SELECT COUNT(*) FROM `public_health_portfolio.tbl_nndss_state_fact`) AS fact_rows,
-  (SELECT COUNT(*) FROM `public_health_portfolio.tbl_tableau_export`)  AS export_rows;
